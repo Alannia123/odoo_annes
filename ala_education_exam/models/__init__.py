f9f61076dd06954ab8b86a8a9f70d72e99c6e2f5
Arockiadass <arockiadass@ifensys.com>
2026-07-27 08:55:09 +0530

    sgf

--- ala_education_exam/models/__init__.py
+++ ala_education_exam/models/__init__.py
@@ -6,5 +6,5 @@ from . import education_exam_valuation
 # from . import student_exam_record
 from . import class_division
 from . import student_evaluation
-from . import report_rank_card
+# from . import report_rank_card
 from . import report_student_marksheet
